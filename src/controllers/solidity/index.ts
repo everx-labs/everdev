c5e514154f0f0db64057f730259d1a23fde3d19f
Michael Vlasov <melsomino@gmail.com>
2021-03-11 19:26:52 +0500

    NEW: Solidity controller has rewritten FIX: `sol update` didn't actually update sometimes

--- src/controllers/solidity/index.ts
+++ src/controllers/solidity/index.ts
@@ -1,11 +1,133 @@
-import { solidityCreateCommand } from "./create";
-import { solidityCompileCommand } from "./compile";
-import { ToolController } from "../../core";
-import { solidityVersionCommand } from "./version";
 import {
-    soliditySetCommand,
-    solidityUpdateCommand,
-} from "./update";
+    Command,
+    Terminal,
+    ToolController,
+} from "../../core";
+import {
+    components,
+} from "./installer";
+import path from "path";
+import {
+    changeExt,
+    uniqueFilePath,
+} from "../../core/utils";
+import fs from "fs";
+import {BasicContract} from "./snippets";
+import {Component} from "./component";
+
+export const solidityVersionCommand: Command = {
+    name: "version",
+    title: "Show Solidity Version",
+    async run(terminal: Terminal, _args: {}): Promise<void> {
+        terminal.log(await Component.getInfoAll(components));
+    },
+};
+
+export const solidityCreateCommand: Command = {
+    name: "create",
+    title: "Create Solidity Contract",
+    args: [{
+        isArg: true,
+        name: "name",
+        title: "Contract Name",
+        type: "string",
+        defaultValue: "Contract",
+    }, {
+        name: "folder",
+        type: "folder",
+    }],
+    async run(terminal: Terminal, args: { name: string, folder: string }) {
+        const filePath = uniqueFilePath(args.folder, `${args.name}{}.sol`);
+        const text = BasicContract.split("{name}").join(args.name);
+        fs.writeFileSync(filePath, text);
+        terminal.log(`Solidity contract ${path.basename(filePath)} created.`);
+    },
+};
+
+
+export const solidityCompileCommand: Command = {
+    name: "compile",
+    title: "Compile Solidity Contract",
+    args: [
+        {
+            isArg: true,
+            name: "file",
+            type: "file",
+            title: "Source file",
+            nameRegExp: /\.sol$/i,
+        },
+    ],
+    async run(terminal: Terminal, args: { file: string }): Promise<void> {
+        terminal.log("Starting build...");
+        const ext = path.extname(args.file);
+        if (ext !== ".sol") {
+            terminal.log(`Choose solidity source file.`);
+            return;
+        }
+        await Component.ensureInstalledAll(terminal, components);
+        const fileDir = path.dirname(args.file);
+        const fileName = path.basename(args.file);
+        const tvcName = changeExt(fileName, ".tvc");
+        const codeName = changeExt(fileName, ".code");
+
+        await components.compiler.run(terminal, fileDir, [fileName]);
+        const linkerOut = await components.linker.run(
+            terminal,
+            fileDir,
+            ["compile", codeName, "--lib", components.stdlib.path]);
+
+        const generatedTvcName = `${/Saved contract to file (.*)$/mg.exec(linkerOut)?.[1]}`;
+        fs.renameSync(path.resolve(fileDir, generatedTvcName), path.resolve(fileDir, tvcName));
+        fs.unlinkSync(path.resolve(fileDir, codeName));
+        terminal.log("Compile complete.");
+    },
+};
+
+export const solidityUpdateCommand: Command = {
+    name: "update",
+    title: "Update Solidity Compiler",
+    async run(terminal: Terminal, _args: {}): Promise<void> {
+        await Component.updateAll(terminal, components);
+    },
+};
+
+export const soliditySetCommand: Command = {
+    name: "set",
+    title: "Change Solidity Config",
+    args: [
+        {
+            name: "compiler",
+            title: "Compiler version (version number or `latest`)",
+            type: "string",
+            defaultValue: "",
+
+        },
+        {
+            name: "linker",
+            title: "Linker version (version number or `latest`)",
+            type: "string",
+            defaultValue: "",
+
+        },
+    ],
+    async run(terminal: Terminal, args: {
+        compiler: string,
+        linker: string,
+    }): Promise<void> {
+        const versions: {
+            compiler?: string,
+            linker?: string,
+        } = {};
+        if (args.compiler !== "") {
+            versions.compiler = args.compiler;
+        }
+        if (args.linker !== "") {
+            versions.linker = args.linker;
+        }
+        await Component.setVersions(terminal, components, versions);
+    },
+};
+
 
 export const Solidity: ToolController = {
     name: "sol",
@@ -15,6 +137,6 @@ export const Solidity: ToolController = {
         solidityCompileCommand,
         solidityVersionCommand,
         soliditySetCommand,
-        solidityUpdateCommand
+        solidityUpdateCommand,
     ],
 };
--- src/controllers/solidity/installer.ts
+++ src/controllers/solidity/installer.ts
@@ -1,192 +1,40 @@
 import path from "path";
 import fs from "fs";
 import {
-    Terminal,
     tondevHome,
 } from "../../core";
-import {
-    compareVersions,
-    downloadFromBinaries,
-    executableName,
-    formatTable,
-    httpsGetJson,
-    nullTerminal,
-    run,
-} from "../../core/utils";
+import {Component} from "./component";
 
 function solidityHome() {
     return path.resolve(tondevHome(), "solidity");
 }
 
-function configPath(): string {
-    return path.resolve(solidityHome(), "config.json");
-}
+export const components = {
+    compiler: new Component(solidityHome(), "solc", {
+        executable: true,
+    }),
 
-export type SolidityConfig = {
-    compilerVersion: string,
-    linkerVersion: string,
-}
+    linker: new Component(solidityHome(), "tvm_linker", {
+        executable: true,
+        extractCurrentVersionRegExp: /TVM linker\s*([0-9.]+)/,
+    }),
 
-export async function getConfig(): Promise<SolidityConfig> {
-    let config: SolidityConfig | null = null;
-    try {
-        if (fs.existsSync(configPath())) {
-            config = JSON.parse(fs.readFileSync(configPath(), "utf8"));
+    stdlib: new class extends Component {
+        // TODO: support versioning
+        getSourceName(_version: string): string {
+            return this.name;
         }
-    } catch {
-    }
-    if (!config) {
-        const {
-            compiler,
-            linker,
-        } = await loadAvailableVersions();
-        config = {
-            compilerVersion: compiler[0],
-            linkerVersion: linker[0],
-        };
-        await setConfig(config);
-    }
-    return config;
-}
 
-export function setConfig(config: SolidityConfig) {
-    const configDir = path.dirname(configPath());
-    if (!fs.existsSync(configDir)) {
-        fs.mkdirSync(configDir, {recursive: true});
-    }
-    fs.writeFileSync(configPath(), JSON.stringify(config, undefined, "    "), "utf8");
-}
+        // TODO: support versioning
+        async getCurrentVersion(): Promise<string> {
+            return fs.existsSync(this.path) ? "1.0.0" : "";
+        }
 
-
-export function compilerPath() {
-    return path.resolve(solidityHome(), executableName("solc"));
-}
-
-export function linkerPath() {
-    return path.resolve(solidityHome(), executableName("tvm_linker"));
-}
-
-export function stdLibPath() {
-    return path.resolve(solidityHome(), "stdlib_sol.tvm");
-}
-
-export async function solidityEnsureInstalled(terminal: Terminal) {
-    if (fs.existsSync(compilerPath())) {
-        return;
-    }
-    const config = await getConfig();
-    terminal.log("Installing solidity compiler...");
-    await downloadFromBinaries(terminal, stdLibPath(), "stdlib_sol");
-    await downloadFromBinaries(
-        terminal,
-        compilerPath(),
-        `solc_${config.compilerVersion.split(".").join("_")}_{p}`, {
-            executable: true,
-        });
-    await downloadFromBinaries(
-        terminal,
-        linkerPath(),
-        `tvm_linker_${config.linkerVersion.split(".").join("_")}_{p}`, {
-            executable: true,
-        });
-    terminal.log("Solidity compiler has been installed.");
-}
-
-export async function soliditySet(terminal: Terminal, update: Partial<SolidityConfig>) {
-    const config = await getConfig();
-    let hasUpdates = false;
-    if (update.compilerVersion && update.compilerVersion !== config.compilerVersion) {
-        config.compilerVersion = update.compilerVersion;
-        hasUpdates = true;
-    }
-    if (update.linkerVersion && update.linkerVersion !== config.linkerVersion) {
-        config.linkerVersion = update.linkerVersion;
-        hasUpdates = true;
-    }
-    if (!hasUpdates) {
-        terminal.log("No changes to solidity config.");
-        return;
-    }
-    if (fs.existsSync(compilerPath())) {
-        fs.unlinkSync(compilerPath());
-    }
-    if (fs.existsSync(linkerPath())) {
-        fs.unlinkSync(linkerPath());
-    }
-    setConfig(config);
-    await solidityEnsureInstalled(terminal);
-    await solidityInfo(terminal);
-}
-
-export async function solidityUpdate(terminal: Terminal) {
-    const {
-        compiler,
-        linker,
-    } = await loadAvailableVersions();
-    await soliditySet(terminal, {
-        compilerVersion: compiler[0],
-        linkerVersion: linker[0],
-    });
-}
-
-async function runTool(terminal: Terminal, toolPath: string, workDir: string, args: string[]): Promise<string> {
-    await solidityEnsureInstalled(terminal);
-    const out = await run(toolPath, args, {cwd: workDir}, terminal);
-    return out.replace(/\r?\n/g, "\r\n");
-}
-
-export async function solc(terminal: Terminal, workDir: string, args: string[]): Promise<string> {
-    return runTool(terminal, compilerPath(), workDir, args);
-}
-
-export async function tvmLinker(terminal: Terminal, workDir: string, args: string[]): Promise<string> {
-    return runTool(terminal, linkerPath(), workDir, args);
-}
-
-export type ComponentVersion = {
-    current: string,
-    available: string[],
-}
-
-export type SolidityVersions = {
-    compiler: ComponentVersion,
-    linker: ComponentVersion,
-}
-
-async function loadVersions(name: string): Promise<string[]> {
-    const versions = (await httpsGetJson(`https://binaries.tonlabs.io/${name}.json`))
-        [name].sort(compareVersions).reverse();
-    return versions.length < 10 ? versions : [...versions.slice(0, 10), "..."];
-}
-
-export async function loadAvailableVersions(): Promise<{ compiler: string[], linker: string[] }> {
-    return {
-        compiler: await loadVersions("solc"),
-        linker: await loadVersions("tvm_linker"),
-    };
-}
-
-export async function getVersions(_terminal: Terminal): Promise<SolidityVersions> {
-    const compilerOut = await solc(nullTerminal, process.cwd(), ["--version"]);
-    const linkerOut = await tvmLinker(nullTerminal, process.cwd(), ["--version"]);
-    const available = await loadAvailableVersions();
-    return {
-        compiler: {
-            current: compilerOut.match(/Version:\s*([0-9.]+)/)?.[1] ?? "",
-            available: available.compiler,
-        },
-        linker: {
-            current: linkerOut.match(/TVM linker\s*([0-9.]+)/)?.[1] ?? "",
-            available: available.linker,
-        },
-    };
-}
-
-export async function solidityInfo(terminal: Terminal): Promise<void> {
-    const version = await getVersions(terminal);
-    terminal.log(formatTable([
-        ["Component", "Version", "Available"],
-        ["Compiler", version.compiler.current, version.compiler.available.join(", ")],
-        ["Linker", version.linker.current, version.linker.available.join(", ")],
-    ], {headerSeparator: true}));
-}
+        // TODO: support versioning
+        async loadAvailableVersions(): Promise<string[]> {
+            return ["1.0.0"];
+        }
+    }(solidityHome(), "stdlib_sol", {
+        targetName: "stdlib_sol.tvm",
+    }),
+};
