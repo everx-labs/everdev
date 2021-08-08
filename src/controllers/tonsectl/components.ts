3cea85dd4e1b67675500466e1755af448c2625d6
Anton Zavodovskiy <azavodovskiy@pixsystem.com>
2021-08-08 23:18:33 +0300

    fix minor bugs for windows

--- src/controllers/tonsectl/components.ts
+++ src/controllers/tonsectl/components.ts
@@ -1,6 +1,19 @@
 import { Component } from "../../core";
+import os from "os";
 
+const p = os.platform();
+let innerPath: string;
 
+if (p === "linux") {
+    innerPath = "tonsectl";
+
+} else if (p === "darwin") {
+    innerPath = "tonsectl";
+
+} else {
+    innerPath = "tonsectl.exe";
+
+}
 
 const TOOL_FOLDER_NAME = "tonsectl";
 
@@ -9,6 +22,7 @@ export const components = {
     }(TOOL_FOLDER_NAME, "tonsectl", {
         isExecutable: true,
         runGlobally: false,
+        innerPath,
     }),
 };
 
